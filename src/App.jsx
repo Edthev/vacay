cf3ec29f0dbf64578bd193f1197e14bbe04357d2
Asden <edthevargas@gmail.com>
2024-09-27 14:31:07 -0400

    import DatePicker

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,15 @@
+import { useState } from "react";
+import DatePicker from "./components/datePicker";
 import "./App.scss";
 
 function App() {
-   return <></>;
+   const [count, setCount] = useState(1);
+   return (
+      <>
+         <DatePicker />
+         <DatePicker />
+      </>
+   );
 }
 
 export default App;
